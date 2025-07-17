05bafc4d86627208b20740550302fd5d7102258e
Abderrahman Gaga <gagabdel@gmail.com>
2025-07-17 12:21:42 +0300

    basic get from the drones api

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+import httpx
+from fastapi import FastAPI, HTTPException
+import os
+from dotenv import load_dotenv
+
 
 app = FastAPI()
+load_dotenv()
+DRONES_API_URL = os.getenv("DRONES_API_URL")
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
+
+@app.get("/drones")
+async def get_drones():
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(DRONES_API_URL)
+            response.raise_for_status()
+            return response.json()
+            
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=f"HTTP error: {str(e)}")
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=503, detail=f"Error connecting to the drones API: {str(e)}")
